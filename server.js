807dc59fcac1a8151c0ecd2faabeaad9165e75cc
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-05-06 16:52:34 -0600

    Get method for specific project with error catch

--- server.js
+++ server.js
@@ -69,3 +69,19 @@ app.post('/api/v1/palettes', (request, response) => {
       response.status(500).json({ error });
     })
 })
+
+app.get('/api/v1/projects/:id', (request, response) => {
+  database('projects').where('id', request.params.id).select()
+    .then(projects => {
+      if (projects.length) {
+        response.status(200).json(projects)
+      } else {
+        response.status(404).json({
+          error: `Could not find a palette with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+})
